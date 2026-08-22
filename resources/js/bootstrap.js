ca0a8df5618d35e3a8c15a1c7aecb026d28dfdb8
APLcuba <https://github.com/APLcuba/antillas-premier-league.git>
2026-08-21 23:46:01 -0400

    Fix: Forzar HTTPS en toda la aplicación

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,4 +2,14 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.baseURL = import.meta.env.VITE_APP_URL || window.location.origin;
+
+// ✅ FORZAR HTTPS EN TODAS LAS PETICIONES
+window.axios.defaults.baseURL = window.location.origin;
+
+// ✅ INTERCEPTOR PARA FORZAR HTTPS
+window.axios.interceptors.request.use(config => {
+    if (config.url && typeof config.url === 'string' && config.url.startsWith('http://')) {
+        config.url = config.url.replace('http://', 'https://');
+    }
+    return config;
+});
